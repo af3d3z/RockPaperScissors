9583417d4dafe635b4dbb84076908a80156b605f
af3d3z <alonso.fernandez@iesnervion.es>
2024-10-30 11:02:29 +0100

    Avances varios

--- app/src/main/java/com/alonso/rockpaperscissors/MainActivity.kt
+++ app/src/main/java/com/alonso/rockpaperscissors/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.alonso.rockpaperscissors
 
+import android.content.Context
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -25,6 +27,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
@@ -49,12 +52,14 @@ class MainActivity : ComponentActivity() {
 }
 
 
+
 @Composable
 fun RPS(modifier: Modifier){
     var iconoMaquina by rememberSaveable {mutableStateOf(R.drawable.bot)}
     var maquina by rememberSaveable { mutableStateOf(0) }
     var jugador by rememberSaveable { mutableStateOf(0) }
     var tiradaMaquina by rememberSaveable{mutableStateOf(0)}
+    var ctx = LocalContext.current
 
     Column (modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         Text(
@@ -98,6 +103,15 @@ fun RPS(modifier: Modifier){
                             2-> iconoMaquina = R.drawable.paper
                             3-> iconoMaquina = R.drawable.scissor
                         }
+                        var puntuacion = checkWinner(playerMove = 1, machineMove = tiradaMaquina, ctx = ctx)
+                        when(puntuacion){
+                            1 -> jugador++
+                            2 -> maquina++
+                        }
+                        if(jugador >= 5 || maquina >= 5){
+                            jugador = 0
+                            maquina = 0
+                        }
                     }
             )
             Image(
@@ -123,4 +137,26 @@ fun RPS(modifier: Modifier){
 
 fun tiradaMaquina(): Int {
     return Random.nextInt(1..3)
+}
+
+/**
+ * Recibe la jugada del jugador y la de la máquina, determina quien gana y si hay empate o no.
+ * @param playerMove movimiento del jugador
+ * @param machineMove movimiento del ordenador
+ * @return Si devuelve 0 es empate, 1 gana el jugador, 2 gana la máquina
+ */
+fun checkWinner(playerMove: Int, machineMove: Int, ctx: Context): Int {
+    var state = 0
+    if(playerMove == 1 && machineMove == 3 || playerMove == 2 && machineMove == 1 || playerMove == 3 && machineMove == 2) {
+        state = 1
+        Toast.makeText(ctx, "Gana el jugador", Toast.LENGTH_LONG).show()
+    }else if(playerMove == 1 && machineMove == 2 || playerMove == 2 && machineMove == 3 || playerMove == 3 && machineMove == 1){
+        state = 2
+        Toast.makeText(ctx, "Gana la máquina", Toast.LENGTH_LONG).show()
+    }
+    else{
+        Toast.makeText(ctx, "Empate", Toast.LENGTH_LONG).show()
+    }
+
+    return state
 }
